426b8831bdbf4817f9d9bdd4912325f9f68d4579
yana-glt <galitsynayasya@gmail.com>
2023-01-08 21:46:08 +0100

    Editing the readFromFile() method

--- src/main/java/com/solvd/banks/FileUtils.java
+++ src/main/java/com/solvd/banks/FileUtils.java
@@ -18,6 +18,54 @@ public class FileUtils {
 
     private final static Logger logger = LogManager.getLogger(FileUtils.class);
 
+    public static void readFromFile(String fileName) {
+        try {
+            File dir = new File(fileName);
+            if (!dir.exists()) {
+                throw new FileNotFoundException(String.format("The file or directory %s does not exist.", dir.getName()));
+            }
+            List<File> files = (List<File>) org.apache.commons.io.FileUtils.listFiles(dir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+            if (files.isEmpty()) {
+                throw new FileIsEmptyException("File or directory is empty", dir.getName());
+            }
+            for (File file : files) {
+                List<String> lines = org.apache.commons.io.FileUtils.readLines(file, StandardCharsets.UTF_8);
+                int numberOfBranches = 0;
+                int numberOfDepartments = 0;
+                int numberOfEmployees = 0;
+                int numberOfClients = 0;
+                int numberOfCredits = 0;
+                int numberOfCards = 0;
+                for (String str : lines) {
+                    if (StringUtils.startsWith(str, "Branch")) {
+                        numberOfBranches++;
+                    }
+                    if (StringUtils.startsWith(str, "Department")) {
+                        numberOfDepartments++;
+                    }
+                    if (StringUtils.startsWith(str, "Employee")) {
+                        numberOfEmployees++;
+                    }
+                    if (StringUtils.startsWith(str, "Client")) {
+                        numberOfClients++;
+                    }
+                    if (StringUtils.startsWith(str, "Credit")) {
+                        numberOfCredits++;
+                    }
+                    if (StringUtils.startsWith(str, "Card")) {
+                        numberOfCards++;
+                    }
+                }
+                System.out.println(String.format("The last time the program was launched, %s branches, %s departments, %s employees, %s clients, %s credits, %s cards were created",
+                        numberOfBranches, numberOfDepartments, numberOfEmployees, numberOfClients, numberOfCredits, numberOfCards));
+            }
+        } catch (FileIsEmptyException e) {
+            logger.error((String.format("File or directory %s is empty", e.getFileOrDirectoryName())), e);
+        } catch (IOException e) {
+            logger.error(String.format("Failed to read information"), e);
+        }
+    }
+
     public static void readFromFile() {
         try {
             File dir = new File("info.txt");
